07a13f55365971fd00997e2986e8a4e26bc3ae0d
DouthapuramPranay <douthapurampranay@gmail.com>
2026-01-02 12:34:19 +0530

    Java Basics part-1

---
+++ MyFirst.java
@@ -0,0 +1,10 @@
+import java.lang.*;
+
+public class MyFirst
+{
+             public static void main(String arg[]){
+                    System.out.println("Hello world");
+                    System.out.println(arg[0]);
+                    System.out.println(arg[1]);
+             }
+}
---
+++ Readinginput.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+
+public class Readinginput{
+      public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        c=sc.nextInt();
+        System.out.print("sum:"+(a+b+c));
+}
+}
+    
+        
