d5c372dbeae031d5e6a986a1fff75604ceb9a96b
DouthapuramPranay <douthapurampranay@gmail.com>
2026-01-20 11:17:50 +0530

    Java Basics part-7

--- Grades_of_a_Student.java
+++ Grades_of_a_Student.java
@@ -3,7 +3,14 @@ import java.util.Scanner;
 public class Grades_of_a_Student {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        int Telugu = sc.nextInt();
+        int Hindi = sc.nextInt();
+        int English = sc.nextInt();
+        int Maths = sc.nextInt();
+        int Science = sc.nextInt();
+        int Social = sc.nextInt();
+
+        float n = (Telugu + Hindi + English + Maths + Science + Social) / 6.0f;
         if (n >= 90) {
             System.out.println("Grade:A");
         } else if (n > 80 && n <= 90) {
